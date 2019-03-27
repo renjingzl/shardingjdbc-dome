3b12bcbfe6fb7d96f1dd547ed1c7948637f52762
renjing <renjing@baojia>
2019-03-27 09:19:32 +0800

    用3.0.0版本

---
+++ src/main/java/com/example/demo/config/BikeOpStatTableShardingAlgorithm.java
@@ -0,0 +1,24 @@
+package com.example.demo.config;
+
+
+import io.shardingsphere.api.algorithm.sharding.PreciseShardingValue;
+import io.shardingsphere.api.algorithm.sharding.standard.PreciseShardingAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
+public class BikeOpStatTableShardingAlgorithm implements PreciseShardingAlgorithm<String> {
+	
+	@Override
+    public String doSharding(Collection<String> availableTargetNames, PreciseShardingValue<String> shardingValue) {
+        for (String tableName : availableTargetNames) {
+            System.out.println("tableName: " + tableName);
+            if (tableName.endsWith(Long.parseLong(shardingValue.getValue().toString()) % 10+"")) {
+                return tableName;
+            }
+        }
+
+        throw new UnsupportedOperationException();
+    }
+}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,7 +2,12 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
