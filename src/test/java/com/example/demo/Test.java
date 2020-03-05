8fbcb4a83eac76ba361323f093f8b1474c4f7f4d
renjing <renjing@baojia>
2020-03-05 15:41:08 +0800

    ConcurrentReferenceHashMap

--- src/test/java/com/example/demo/Test.java
+++ src/test/java/com/example/demo/Test.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.ConcurrentReferenceHashMap;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -15,9 +16,18 @@ import java.util.Set;
 public class Test {
     public static void main(String[] args) {
         String source = "hello morning today today";
-        String target = "abcba";
+        String target = "abcc";
         System.out.println("运算后的结果：" + toStr(source));
         System.out.println("源和目标是否一致：" + target.equals(toStr(source)));
+
+
+        /**
+         * ConcurrentHashMap的弱一致性：https://zhuanlan.zhihu.com/p/38194318
+         * 当一对key和value通过调用put方法存储到ConcurrentHashMap中，
+         * 但是另外一个线程想通过该key马上调用get()方法(get方法不需要获取锁)获取的value的时候，
+         * 可能获取到的是一个null。因为它必须等到释放锁之后，才会将最新的值更新到主存中
+        */
+        getValue();
     }
 
     public static String toStr(String source) {
@@ -43,5 +53,33 @@ public class Test {
             }
         }
         return sb.toString();
+    }
+
+
+
+
+
+
+    static ConcurrentReferenceHashMap<String, Integer[]> map = new ConcurrentReferenceHashMap<>(2,ConcurrentReferenceHashMap.ReferenceType.SOFT);
+
+    public static Integer getValue() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                map.put("aa", new Integer[]{Integer.valueOf("12")});
+                System.out.println(Thread.currentThread().getName() + ":"  + map.get("aa")[0]);
+            }
+        }).start();
+
+        //这里要暂停一会儿mian线程才能获取到值
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(Thread.currentThread().getName() + ":" + map.get("aa")[0]);
+        return map.get("aa")[0];
     }
 }
