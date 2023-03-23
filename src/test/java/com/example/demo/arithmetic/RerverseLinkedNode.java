f313207bd317b6bea9d3e3f3dbdc48b9acc0689c
renjing <renjing@baojia>
2023-03-23 18:38:03 +0800

    update

--- src/test/java/com/example/demo/arithmetic/RerverseLinkedNode.java
+++ src/test/java/com/example/demo/arithmetic/RerverseLinkedNode.java
@@ -3,6 +3,7 @@ package com.example.demo.arithmetic;
 
 import lombok.Data;
 
+
 /**
  * @author renjing
  * @Description:
@@ -13,16 +14,20 @@ public class RerverseLinkedNode {
     public static void main(String[] args) {
         Node node1 = new Node(1);
         Node node2 = new Node(2);
-        Node node3 = new Node(3);
+        Node node3 = new Node(1);
         Node node4 = new Node(4);
 
         node1.next = node2;
         node2.next = node3;
         node3.next = node4;
 
-        print(reverse(node1));
+//        print(reverse(node1));
+//        System.out.println(checkCycle(node1));
     }
 
+    /**
+     * 翻转单链表
+     */
     public static Node reverse(Node head) {
         Node pre = null;
         Node next = null;
@@ -33,6 +38,25 @@ public class RerverseLinkedNode {
             head = next;
         }
         return pre;
+    }
+
+    /**
+     * 快慢指针判断链表有环
+    */
+    public static boolean checkCycle(Node head) {
+        if(head == null || head.next == null) {
+            return false;
+        }
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void print(Node node) {
